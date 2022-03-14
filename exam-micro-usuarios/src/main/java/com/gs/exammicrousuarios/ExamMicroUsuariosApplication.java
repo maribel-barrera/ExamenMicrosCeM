a9fb710810cd632e4236e4543dfb760923f05bfd
Maribel Barrera Tabares <1018marybarrera@gmail.com>
2022-03-13 20:02:43 -0600

    JARS
    Se crea el jar de cada microservice

--- exam-micro-usuarios/src/main/java/com/gs/exammicrousuarios/ExamMicroUsuariosApplication.java
+++ exam-micro-usuarios/src/main/java/com/gs/exammicrousuarios/ExamMicroUsuariosApplication.java
@@ -2,10 +2,10 @@ package com.gs.exammicrousuarios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableFeignClients
 public class ExamMicroUsuariosApplication {
 
 	public static void main(String[] args) {
